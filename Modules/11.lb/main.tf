a0c31a6b1d7fc4613c9dbe897d0a42a4a5eac856
Ubuntu <cptiwari@Cptiwari-vm-donot-delete-please.z0dyuvery2leheyht35qvpuiua.ax.internal.cloudapp.net>
2025-03-11 10:03:04 +0000

    update

--- Modules/11.lb/main.tf
+++ Modules/11.lb/main.tf
@@ -22,42 +22,41 @@ resource "azurerm_lb" "loadbalancer" {
 
 # Create a backend pool for the Load Balancer and populate it with VM IPs
 resource "azurerm_lb_backend_address_pool" "bkpool-address" {
-  
+  for_each = var.RG
   name                           = each.value.backend_pool_name
-  loadbalancer_id                = azurerm_lb.loadbalancer.id
+  loadbalancer_id                = azurerm_lb.loadbalancer[each.key].id
   }
 
 resource "azurerm_lb_backend_address_pool_address" "backendnginx01" {
-  
+  for_each = var.RG
   name                    = each.value.lb_bkpool-addresses_name-01
-  backend_address_pool_id = azurerm_lb_backend_address_pool.bkpool-address.id
-  virtual_network_id      = data.azurerm_virtual_network.vnet_block.id
-  ip_address              = data.azurerm_network_interface.nic_block.private_ip_address
+  backend_address_pool_id = azurerm_lb_backend_address_pool.bkpool-address[each.key].id
+  virtual_network_id      = data.azurerm_virtual_network.vnet_block[each.key].id
+  ip_address              = data.azurerm_network_interface.nic_block[each.key].private_ip_address
 }
 
 resource "azurerm_lb_backend_address_pool_address" "backendnginx02" {
-  
+  for_each = var.RG
   name                    = each.value.lb_bkpool-addresses_name-02
-  backend_address_pool_id = azurerm_lb_backend_address_pool.bkpool-address.id
-  virtual_network_id      = data.azurerm_virtual_network.data-vnet.id
-  ip_address              = data.azurerm_network_interface.nic_block.private_ip_address
+  backend_address_pool_id = azurerm_lb_backend_address_pool.bkpool-address[each.key].id
+  virtual_network_id      = data.azurerm_virtual_network.vnet_block[each.key].id
+  ip_address              = data.azurerm_network_interface.nic_block[each.key].private_ip_address
 }
-  resource "azurerm_lb_probe" "nginxprobe" {
-    
-  loadbalancer_id = azurerm_lb.loadbalancer.id
+  resource "azurerm_lb_probe" "lbprobe" {
+    for_each = var.RG
+  loadbalancer_id = azurerm_lb.loadbalancer[each.key].id
   name            = "http-port"
   port            = 80
   
 }
 resource "azurerm_lb_rule" "example" {
   for_each = var.RG
-  loadbalancer_id                = azurerm_lb.loadbalancer.id
+  loadbalancer_id                = azurerm_lb.loadbalancer[each.key].id
   name                           = "NginxRule"
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = each.value.frontend_pip_name
-  
-  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bkpool-address.id]
-  probe_id                       = azurerm_lb_probe.nginxprobe.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bkpool-address[each.key].id]
+  probe_id                       = azurerm_lb_probe.lbprobe[each.key].id
 }
